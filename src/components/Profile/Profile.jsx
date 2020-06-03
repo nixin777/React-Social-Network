f824d28e7a955847c66353ed3083beb73e00ffb1
Nixin <nixin@yandex.ru>
2020-06-04 00:23:35 +0300

    Dialogs page, route, browser-router, routing

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "../MyPosts/MyPosts";
 
 const Profile = () => {
     return(
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='https://avatars.mds.yandex.net/get-pdb/907231/09738fd8-6068-4d90-902f-c3c2733bfa6d/s1200?webp=false'/>
             </div>
