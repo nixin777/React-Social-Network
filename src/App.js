87ffd707cafb74173d34fb9dcceac87f54260d89
nixin777 <nixin@yandex.ru>
2020-07-13 17:27:11 +0000

    Page Users

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
+
 
 function App(props) {
     return (
@@ -19,6 +20,9 @@ function App(props) {
                 />
                 <Route path='/profile'
                        render={() => <Profile />}
+                />
+                <Route path='/users'
+                       render={() => <UsersContainer />}
                 />
             </div>
         </div>
